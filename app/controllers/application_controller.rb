a1f4930855ef3e8ae2d249c6ad1fa57cde780606
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 14:40:35 +0000

    Added navbar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+  include DefaultPageContent
+  
+  
+  before_filter :set_title
+  
+  def set_title
+    @page_title = "Devcamp Portfolio | My Portfolio Website"
+  end
 end
