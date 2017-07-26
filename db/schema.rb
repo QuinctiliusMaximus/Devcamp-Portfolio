378c39eb7b8804afb3ee4ac6bf03d3c83acb7142
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 15:59:44 +0000

    Added installation requirements for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170706144736) do
-
+ActiveRecord::Schema.define(version: 20170726155630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,8 +69,6 @@ ActiveRecord::Schema.define(version: 20170706144736) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -89,9 +85,9 @@ ActiveRecord::Schema.define(version: 20170706144736) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "roles"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   end
 
   add_foreign_key "blogs", "topics"
